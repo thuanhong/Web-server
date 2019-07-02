3f45704d7ae0ed729697270390fe16f231c6879a
HongThuan <hongthuan@localhost.localdomain>
2019-07-01 23:16:39 -0400

    create first page

--- pythonweb/moviepoll/views.py
+++ pythonweb/moviepoll/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World, Movie Poll")
+    my_dict = {'text':"this is django"}
+    return render(request, 'index.html', context=my_dict)
--- pythonweb/pythonweb/urls.py
+++ pythonweb/pythonweb/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from moviepoll import views
 
 urlpatterns = [
-    path('moviepoll/', include('moviepoll.urls')),
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
