9136591088704eef8bd2e33905faa71518987d6e
HongThuan <hongthuan@localhost.localdomain>
2019-07-05 14:28:24 -0400

    upgrade config

--- pythonweb/moviepoll/views.py
+++ pythonweb/moviepoll/views.py
@@ -1,16 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question
 
 # Create your views here.
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    latest_question_list = Question.objects.order_by('-pub_date')[::-1]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'index.html', context)
+
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'detail.html', {'question': question})
 
 
 def results(request, question_id):
