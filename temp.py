151f6df60171ba8bce3c3434d34b2c15ff2c3a9f
HongThuan <hongthuan@localhost.localdomain>
2019-07-07 22:23:07 -0400

    finally

--- temp.py
+++ temp.py
@@ -1,19 +1,26 @@
 import unittest
 from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
 
 
 class PythonTest(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Firefox()
+        self.driver.get("http://127.0.0.1:8000/")
     
     def tearDown(self):
         self.driver.close()
         
     def test_title_homepage(self):
-        driver = self.driver
-        self.driver.get("http://127.0.0.1:8000/")
-        self.assertEquals("Home Page", driver.title)
+        self.assertEqual("Home Page", self.driver.title)
+        
+    def test_button(self):
+        btn = self.driver.find_element_by_id('hp')
+        btn.click()
+        
+    def test_vote(self):
+        self.test_button()
+        self.assertIn("Intek", self.driver.title)
+    
 
 if __name__ == '__main__':
     unittest.main()
